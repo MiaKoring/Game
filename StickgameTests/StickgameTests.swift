acbabfcd4b8c30aeaa7208a8252e6a0bd52a140d
Mia Koring <koring.mia@gmail.com>
2025-08-06 13:42:47 +0200

    Initial Commit

---
+++ StickgameTests/StickgameTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickgameTests.swift
+//  StickgameTests
+//
+//  Created by Mia Koring on 06.08.25.
+//
+
+import Testing
+@testable import Stickgame
+
+struct StickgameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
